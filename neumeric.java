546ddfb25bba33048685128e126b4a7def796e44
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-08-09 11:09:11 +0530

    Create neumeric.java

---
+++ neumeric.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+             	Scanner h = new Scanner (System.in);
+		
+		String str=h.next();
+		char k;
+		int i;
+		int count=0;
+		for(i=0;i<str.length();i++)
+		{
+			k=str.charAt(i);
+			if(Character.isLetter(k))
+			{
+				count=1;
+			}
+		}
+		if(count==1)
+			System.out.println("No");
+		else
+		System.out.println("yes");
+
+	}
+}
+		
