11ddc3d8ce18ab7fd73b34543a5e822e7a16ab20
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-08-09 20:31:54 +0530

    Create digitcount.java

---
+++ digitcount.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner (System.in);
+		int n = s.nextInt();
+		int r=0,count = 0;
+		while (n > 0)
+		{
+			r = r%10;
+			count+=1;
+			n = n/10;
+		}
+		System.out.println(count);
+	}
+}
