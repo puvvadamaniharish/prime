90f8a902cdc63895287157bbf3b21dbc35c5d71d
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-08-13 11:43:42 +0530

    Create wordcount.java

---
+++ wordcount.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,a,count=0;
+		String str1;
+		Scanner s=new Scanner(System.in);
+		str=s.nextLine();
+	            for(i=0;i<str1.length();i++)
+	            {
+	            	if(str1.charAt(i)==' ')
+	            	count++;
+	            }
+	            ++count;
+		System.out.println(count);
+	}
+}
